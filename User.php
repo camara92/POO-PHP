08bfce3b86a63c48e0beb06d4262ac93c929cc24
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-11 17:44:10 +0200

    exo

--- User.php
+++ User.php
@@ -1,3 +1,41 @@
 <?php
-    
-?>
+
+use User as GlobalUser;
+
+class User
+{
+    public $name;
+    public $age;
+    public $avatar;
+
+
+    function choixAvatar($choice)
+    {
+        $avatar = ["😜", "😎", "🤣", "❤️"];
+
+        // return $choice; 
+        return $this->avatar = $avatar[$choice];
+    }
+
+    function tchater($message)
+    {
+        echo "<p> - $this->avatar | $this->name : $message </p>";
+    }
+}
+// creation de data 
+$michel = new User();
+$michel->name = "Michoux";
+$michel->age = 77;
+$michel->choixAvatar(2);
+//echo $michel->avatar;
+echo $michel->tchater("Il est pas si difficile quand c'est la correction ! ");
+
+//une deuxieme personne 
+$daouda = new User();
+$daouda->name ="Daouda";
+$daouda->age = 25;
+$daouda->choixAvatar(1);
+//echo $daouda->avatar;
+echo $daouda->tchater("Belob quand ça déplaise : facile game ! ");
+
+
