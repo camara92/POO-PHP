a62edc79c8e15aa06747d68e6f5a89e31f440c51
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-12 15:01:49 +0200

    Suite construct en PHP

--- Methodes/index.php
+++ Methodes/index.php
@@ -1,15 +1,36 @@
 <?php
-    class Verre {
-    function __construct(){
+class Verre
+{
+    public $nom;
+    public $materiaux;
+    public $contenu;
+    function __construct($nom, $materiaux, $contenu)
+    {
 
         //se déclenche sur l'instanciain de l'objet 
-        echo "<p> Création de l'objet <p>"; 
+        //echo "<p> Création de l'objet <p>"; 
+
+        $this->nom = $nom;
+        $this->materiaux = $materiaux;
+        $this->contenu = $contenu;
     }
-
-
 }
-$gobelet= new Verre();
-$gobelet= new Verre();
+$gobelet = new Verre("Gobelet", "Plastique", "Eau");
+$mug = new Verre("Mug", "Métal", "Slug");
+class Voiture
+{
+    public $modele;
+    public $couleur;
+    function __construct($modele, $couleur)
+    {
+        $this->modele = $modele;
+        $this->couleur = $couleur;
+        echo "Une $this->modele $this->couleur a été créée";
+    }
+}
+$pagani = new Voiture("Pagani Zodnda", "Rouge"); 
 
-
-
+// $mug->nom ="Mug"; 
+// $mug->materiaux ="Métal"; 
+// $mug->contenu = "Lait"; 
+// echo $gobelet;
