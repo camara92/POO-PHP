b3787f599aacab244ee76efbb9052551c3f47515
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-12 15:09:18 +0200

    Explication construct

--- Methodes/index.php
+++ Methodes/index.php
@@ -4,6 +4,8 @@ class Verre
     public $nom;
     public $materiaux;
     public $contenu;
+    public $prix; 
+    public $couleur; 
     function __construct($nom, $materiaux, $contenu)
     {
 
@@ -13,22 +15,32 @@ class Verre
         $this->nom = $nom;
         $this->materiaux = $materiaux;
         $this->contenu = $contenu;
+     
     }
 }
 $gobelet = new Verre("Gobelet", "Plastique", "Eau");
+$gobelet->couleur ="Blanche"; 
+
+//niveau 1 : construct utilisé pouir ocnstruuire un objet 
+//niveau 2 : se déclenche à l'initialisation de l'objet 
+// niveau 3 la methode magique __construct peut avoir un ou non des paramètres 
+
+//niveau 4 : une class peut avoir un ou non un constructeur 
+
 $mug = new Verre("Mug", "Métal", "Slug");
-class Voiture
-{
-    public $modele;
-    public $couleur;
-    function __construct($modele, $couleur)
-    {
-        $this->modele = $modele;
-        $this->couleur = $couleur;
-        echo "Une $this->modele $this->couleur a été créée";
-    }
-}
-$pagani = new Voiture("Pagani Zodnda", "Rouge"); 
+// class Voiture
+// {
+//     public $modele;
+//     public $couleur;
+//     function __construct($modele, $couleur)
+//     {
+//         $this->modele = $modele;
+//         $this->couleur = $couleur;
+//         echo "Une $this->modele $this->couleur a été créée";
+//     }
+// }
+// $pagani = new Voiture("Pagani Zodnda", "Rouge"); 
+// var_dump($gobelet); 
 
 // $mug->nom ="Mug"; 
 // $mug->materiaux ="Métal"; 
